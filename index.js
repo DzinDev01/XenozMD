b0668b4e2de6f2ad7729c880dc2d0a6d606357a4
DzinDev01 <dzingithub01@gmail.com>
2025-02-15 08:59:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("http").createServer((_, res) => res.end("Uptime!")).listen(8080)
 require('./database/settings')
 const fs = require('fs')
 const pino = require('pino')
@@ -23,7 +24,6 @@ async function DzinStart() {
   const { state, saveCreds } = await useMultiFileAuthState('session') 
   
   const dzin = await WAConnection({ 
-    require("http").createServer((_, res) => res.end("Uptime!")).listen(8080)
     version: [2, 3000, 1017531287], 
     browser: ['ios', 'Chrome', '10.15.7'], 
     printQRInTerminal: !pairingCode, 
